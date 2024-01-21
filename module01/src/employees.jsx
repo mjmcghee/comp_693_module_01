52887041d634893d26fc0084d1049d4e4c7fb2cd
mjmcghee <107767198+mjmcghee@users.noreply.github.com>
2024-01-21 01:47:25 -0800

    Add files via upload

---
+++ module01/src/employees.jsx
@@ -0,0 +1,23 @@
+let sally = 'Sally Smith'
+let mark = 'Mark Martin'
+let holly = 'Holly Unlikely'
+let amol = 'Amol Shookup'
+let dan = 'Dan Johnson'
+let shamus = 'Shamus Izaak'
+let aaron = 'Aaron Tyres'
+let marcus = 'Marcus McGhee'
+let ice = 'Ice Spice'
+const element = (
+    <ul style={{'color':'blue', 'fontSize': '24px'}}>
+        <li>{sally}</li>
+        <li>{mark}</li>
+        <li>{holly}</li>
+        <li>{amol}</li>
+        <li>{dan}</li>
+        <li>{shamus}</li>
+        <li>{aaron}</li>
+        <li>{marcus}</li>
+        <li>{ice}</li>
+    </ul>
+)
+ReactDOM.render(element, document.getElementById('content'))
